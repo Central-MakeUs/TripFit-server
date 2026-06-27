d065f6d331e218ef6032fc65994104fac9f83ace
chhezue <chhezue@gmail.com>
2026-06-27 19:29:10 +0900

    Init: First Commit

---
+++ src/main/java/com/tripfit/tripfit/TripfitApplication.java
@@ -0,0 +1,13 @@
+package com.tripfit.tripfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripfit/tripfit/TripfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripfit.tripfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
